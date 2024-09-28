5e474d7dd2bf41cc64586fbb1dad83a87d294615
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-09-28 11:54:47 +0100

    boot commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "2024-daw-leic52d-im-i52d-2425-g04"
+include("domain")
+include("service")
+include("repository")
