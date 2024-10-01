3e4c6168629c3cae5f1418fc1ab923f6c22387a6
mariana-moraes <A50560@alunos.isel.pt>
2024-10-01 10:39:43 +0100

    -Message domain first implementation + tests

--- domain/src/main/kotlin/model/Message.kt
+++ domain/src/main/kotlin/model/Message.kt
@@ -1,5 +1,25 @@
 package model
 
+import java.time.LocalDateTime
+
+/**
+ * Represents a Message.
+ *
+ * @property msgId The unique identifier of the message.
+ * @param msg the message.
+ * @param user the user that created the message.
+ * @param channel the channel in which it is present.
+ * @param creationTime the timestamp of when the message is created.
+ */
+
 data class Message(
-	val dummy: String
-)
+	val msgId: UInt? = null,
+	val msg: String,
+	val user: UserInfo,
+	val channel: ChannelName,
+	val creationTime: LocalDateTime
+){
+	init {
+		require(msg.isNotBlank()) { "The message cannot be empty." }
+	}
+}
