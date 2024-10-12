61646648ebf5afccf4d93f5f889adee9a1679af0
Brgm37 <brgm37@gmail.com>
2024-10-12 23:26:55 +0100

    UserInputModel refactored into UserSignUpInputModel and UserLogInInputModel.

--- code/jvm/httpApi/src/main/kotlin/com/example/appWeb/model/dto/input/user/UserLogInInputModel.kt
+++ code/jvm/httpApi/src/main/kotlin/com/example/appWeb/model/dto/input/user/UserLogInInputModel.kt
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.Pattern
 import model.Password
 
 /**
- * Represents the input model for a user
- * @param username The username
- * @param password The password
+ * Represents the user input model for the log-in operation.
+ * @param username The username of the user to log in
+ * @param password The password of the user to log in
  */
-data class UserInputModel(
+data class UserLogInInputModel(
 	@get:NotBlank val username: String,
 	@get:NotBlank @get:Pattern(regexp = Password.PASSWORD_PATTERN)
 	val password: String,
---
+++ code/jvm/httpApi/src/main/kotlin/com/example/appWeb/model/dto/input/user/UserSignUpInputModel.kt
@@ -0,0 +1,20 @@
+package com.example.appWeb.model.dto.input.user
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import model.Password
+
+/**
+ * Represents the user input model for the sign-up operation.
+ * @param username The username of the user
+ * @param password The password of the user
+ * @param invitationCode The invitation code to join the app
+ * @param inviterUId The inviter user id
+ */
+data class UserInputSignUpModel(
+	@get:NotBlank val username: String,
+	@get:NotBlank @get:Pattern(regexp = Password.PASSWORD_PATTERN)
+	val password: String,
+	@get:NotBlank val invitationCode: String,
+	val inviterUId: UInt,
+)
