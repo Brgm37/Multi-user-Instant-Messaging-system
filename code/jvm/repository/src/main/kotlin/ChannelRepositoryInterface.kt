3a1345bf7e56c812d085a47f267c9fdfe0d79157
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-10-12 17:27:07 +0100

    Ktlint changes on repository module.

--- code/jvm/repository/src/main/kotlin/ChannelRepositoryInterface.kt
+++ code/jvm/repository/src/main/kotlin/ChannelRepositoryInterface.kt
@@ -14,7 +14,23 @@ interface ChannelRepositoryInterface : Repository<Channel> {
 	/**
 	 * Retrieves all channels owned by a user
 	 * @param userId The ID of the user
+	 * @param offset The offset to start retrieving channels
+	 * @param limit The maximum number of channels to retrieve
 	 * @return A list with all channels owned by the user
 	 */
-	fun findByUserId(userId: UInt): List<Channel>
+	fun findByUserId(
+		userId: UInt,
+		offset: Int,
+		limit: Int,
+	): List<Channel>
+
+	/**
+	 * Joins a user to a channel
+	 * @param channelId The ID of the channel
+	 * @param userId The ID of the user
+	 */
+	fun joinChannel(
+		channelId: UInt,
+		userId: UInt,
+	)
 }
