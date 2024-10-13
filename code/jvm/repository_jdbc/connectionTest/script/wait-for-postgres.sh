52446e88dacbfb400eae94d9600937dd36fb5bb5
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-10-13 11:20:34 +0100

    wait-for-postgres.sh file update.

--- code/jvm/repository_jdbc/connectionTest/script/wait-for-postgres.sh
+++ code/jvm/repository_jdbc/connectionTest/script/wait-for-postgres.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
-# specify the shell for the script
+# wait-for-postgres.sh host cmd
 
 set -e
-# set the shell to exit immediately if a command exits with a non-zero status
 
 host="$1"
-# set the host variable to the first argument passed to the script
 shift
-# remove the first argument from the list of arguments
-cmd="$*"
-# set the cmd variable to the remaining arguments
+cmd="$@"
 
 >&2 echo "waiting for postgres on $host"
-until pg_isready -h "$host"; do
-# until the pg_isready command returns true
+until pg_isready -h $host; do
   >&2 echo "Postgres is unavailable - sleeping"
-  # print a message to stderr
   sleep 1
-  # sleep for 1 second
 done
 
 >&2 echo "Postgres is up - executing command '$cmd'"
-# print a message to stderr
-exec "$cmd"
-# execute the command passed to the script
+exec $cmd
