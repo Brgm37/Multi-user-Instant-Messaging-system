06276e77c7494f4b5afe3c2b0b121bda6ec203fc
Brgm37 <brgm37@gmail.com>
2024-10-14 23:25:02 +0100

    ChannelController and UserController are now RestController instead of Controller.

--- code/jvm/httpApi/src/main/kotlin/com/example/appWeb/controller/ChannelController.kt
+++ code/jvm/httpApi/src/main/kotlin/com/example/appWeb/controller/ChannelController.kt
@@ -16,12 +16,12 @@ import jakarta.validation.Valid
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import utils.Failure
 import utils.Success
 
@@ -30,85 +30,85 @@ import utils.Success
  *
  * @param channelService The channel service
  */
-@Controller
+@RestController
 class ChannelController
-    @Inject
-    constructor(
-        @Named("ChannelServices") private val channelService: ChannelServicesInterface,
-    ) {
-        @GetMapping(CHANNEL_ID_URL)
-        fun getChannel(
-            @PathVariable channelId: UInt,
-        ) {
-            when (val response = channelService.getChannel(channelId)) {
-                is Success -> {
-                    ResponseEntity.ok(ChannelOutputModel.fromDomain(response.value))
-                }
+	@Inject
+	constructor(
+		@Named("ChannelServices") private val channelService: ChannelServicesInterface,
+	) {
+		@GetMapping(CHANNEL_ID_URL)
+		fun getChannel(
+			@PathVariable channelId: UInt,
+		) {
+			when (val response = channelService.getChannel(channelId)) {
+				is Success -> {
+					ResponseEntity.ok(ChannelOutputModel.fromDomain(response.value))
+				}
 
-                is Failure -> {
-                    ChannelProblem.ChannelNotFound.response(NOT_FOUND)
-                }
-            }
-        }
+				is Failure -> {
+					ChannelProblem.ChannelNotFound.response(NOT_FOUND)
+				}
+			}
+		}
 
-        @GetMapping(CHANNEL_BASE_URL)
-        fun getChannels(
-            @RequestParam offset: Int = 0,
-            @RequestParam limit: Int = 10,
-        ) {
-            when (val response = channelService.getChannels(offset, limit)) {
-                is Failure -> {
-                    ChannelProblem.ChannelNotFound.response(NOT_FOUND)
-                }
+		@GetMapping(CHANNEL_BASE_URL)
+		fun getChannels(
+			@RequestParam offset: Int = 0,
+			@RequestParam limit: Int = 10,
+		) {
+			when (val response = channelService.getChannels(offset, limit)) {
+				is Failure -> {
+					ChannelProblem.ChannelNotFound.response(NOT_FOUND)
+				}
 
-                is Success -> {
-                    ResponseEntity
-                        .ok(
-                            response
-                                .value
-                                .map(ChannelListOutputModel::fromDomain),
-                        )
-                }
-            }
-        }
+				is Success -> {
+					ResponseEntity
+						.ok(
+							response
+								.value
+								.map(ChannelListOutputModel::fromDomain),
+						)
+				}
+			}
+		}
 
-        @PostMapping(CHANNEL_BASE_URL)
-        fun createChannel(
-            @Valid @RequestBody channel: CreateChannelInputModel,
-        ) {
-            val response =
-                channelService.createChannel(
-                    owner = channel.owner,
-                    name = channel.name,
-                    visibility = channel.visibility,
-                    accessControl = channel.accessControl,
-                )
-            when (response) {
-                is Success -> {
-                    ResponseEntity.ok(ChannelOutputModel.fromDomain(response.value))
-                }
+		@PostMapping(CHANNEL_BASE_URL)
+		fun createChannel(
+			@Valid @RequestBody channel: CreateChannelInputModel,
+		) {
+			val response =
+				channelService.createChannel(
+					owner = channel.owner,
+					name = channel.name,
+					visibility = channel.visibility,
+					accessControl = channel.accessControl,
+				)
+			when (response) {
+				is Success -> {
+					ResponseEntity.ok(ChannelOutputModel.fromDomain(response.value))
+				}
 
-                is Failure -> {
-                    when (response.value) {
-                        InvalidChannelInfo -> ChannelProblem.InvalidChannelInfo.response(BAD_REQUEST)
-                        InvalidChannelAccessControl -> ChannelProblem.InvalidChannelAccessControl.response(BAD_REQUEST)
-                        InvalidChannelVisibility -> ChannelProblem.InvalidChannelVisibility.response(BAD_REQUEST)
-                        UserNotFound -> Problem.UserNotFound.response(NOT_FOUND)
-                        else -> ChannelProblem.UnableToCreateChannel.response(BAD_REQUEST)
-                    }
-                }
-            }
-        }
+				is Failure -> {
+					when (response.value) {
+						InvalidChannelInfo -> ChannelProblem.InvalidChannelInfo.response(BAD_REQUEST)
+						InvalidChannelAccessControl -> ChannelProblem.InvalidChannelAccessControl.response(BAD_REQUEST)
+						InvalidChannelVisibility -> ChannelProblem.InvalidChannelVisibility.response(BAD_REQUEST)
+						UserNotFound -> Problem.UserNotFound.response(NOT_FOUND)
+						else -> ChannelProblem.UnableToCreateChannel.response(BAD_REQUEST)
+					}
+				}
+			}
+		}
 
-        companion object {
-            /**
-             * The base URL for the channel endpoints.
-             */
-            const val CHANNEL_BASE_URL = "/channels"
+		companion object {
+			/**
+			 * The base URL for the channel endpoints.
+			 */
+			const val CHANNEL_BASE_URL = "/channels"
 
-            /**
-             * The URL for the channel with the given id.
-             */
-            const val CHANNEL_ID_URL = "$CHANNEL_BASE_URL/{channelId}"
-        }
-    }
+			/**
+			 * The URL for the channel with the given id.
+			 */
+			const val CHANNEL_ID_URL = "$CHANNEL_BASE_URL/{channelId}"
+		}
+	}
--- code/jvm/httpApi/src/main/kotlin/com/example/appWeb/controller/UserController.kt
+++ code/jvm/httpApi/src/main/kotlin/com/example/appWeb/controller/UserController.kt
@@ -6,6 +6,7 @@ import com.example.appWeb.model.dto.output.user.UserInfoOutputModel
 import com.example.appWeb.model.problem.ChannelProblem
 import com.example.appWeb.model.problem.Problem
 import errors.ChannelError.ChannelNotFound
+import errors.ChannelError.InvitationCodeMaxUsesReached
 import errors.UserError.InvalidUserInfo
 import errors.UserError.InvitationCodeHasExpired
 import errors.UserError.InvitationCodeIsInvalid
@@ -15,17 +16,19 @@ import errors.UserError.UserNotFound
 import interfaces.UserServicesInterface
 import jakarta.inject.Inject
 import jakarta.inject.Named
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import utils.Failure
 import utils.Success
 
@@ -33,91 +36,107 @@ import utils.Success
  * Represents the controller for the user
  * @param userService The user service
  */
-@Controller
+@RestController
 class UserController
-    @Inject
-    constructor(
-        @Named("UserServices") private val userService: UserServicesInterface,
-    ) {
-        @PostMapping(USER_BASE_URL)
-        fun createUser(
-            @Valid @RequestBody user: UserSignUpInputModel,
-        ) {
-            val response =
-                userService.createUser(
-                    username = user.username,
-                    password = user.password,
-                    invitationCode = user.invitationCode,
-                    inviterUId = user.inviterUId,
-                )
-            when (response) {
-                is Success -> {
-                    ResponseEntity.ok(UserAuthenticationOutputModel.fromDomain(response.value))
-                }
+	@Inject
+	constructor(
+		@Named("UserServices") private val userService: UserServicesInterface,
+	) {
+		@PostMapping(USER_BASE_URL)
+		fun createUser(
+			@Valid @RequestBody user: UserSignUpInputModel,
+			res: HttpServletResponse,
+		) {
+			val response =
+				userService.createUser(
+					username = user.username,
+					password = user.password,
+					invitationCode = user.invitationCode,
+					inviterUId = user.inviterUId,
+				)
+			when (response) {
+				is Success -> {
+					setCookie(response.value.token.toString(), res) // TODO : check if this is the correct way to set cookie
+					ResponseEntity.ok(UserAuthenticationOutputModel.fromDomain(response.value))
+				}
 
-                is Failure -> {
-                    when (response.value) {
-                        InvalidUserInfo -> Problem.InvalidUserInfo.response(BAD_REQUEST)
-                        UserAlreadyExists -> Problem.UserAlreadyExists.response(BAD_REQUEST)
-                        InviterNotFound -> Problem.InviterNotFound.response(BAD_REQUEST)
-                        InvitationCodeIsInvalid -> Problem.InvitationCodeIsInvalid.response(BAD_REQUEST)
-                        InvitationCodeHasExpired -> Problem.InvitationCodeHasExpired.response(BAD_REQUEST)
-                        else -> Problem.UnableToCreateUser.response(BAD_REQUEST)
-                    }
-                }
-            }
-        }
+				is Failure -> {
+					when (response.value) {
+						InvalidUserInfo -> Problem.InvalidUserInfo.response(BAD_REQUEST)
+						UserAlreadyExists -> Problem.UserAlreadyExists.response(BAD_REQUEST)
+						InviterNotFound -> Problem.InviterNotFound.response(BAD_REQUEST)
+						InvitationCodeIsInvalid -> Problem.InvitationCodeIsInvalid.response(BAD_REQUEST)
+						InvitationCodeHasExpired -> Problem.InvitationCodeHasExpired.response(BAD_REQUEST)
+						else -> Problem.UnableToCreateUser.response(BAD_REQUEST)
+					}
+				}
+			}
+		}
 
-        @GetMapping(USER_ID_URL)
-        fun getUser(
-            @PathVariable userId: UInt,
-        ) {
-            when (val response = userService.getUser(userId)) {
-                is Success -> {
-                    ResponseEntity.ok(UserInfoOutputModel.fromDomain(response.value))
-                }
+		@GetMapping(USER_ID_URL)
+		fun getUser(
+			@PathVariable userId: UInt,
+		) {
+			when (val response = userService.getUser(userId)) {
+				is Success -> {
+					ResponseEntity.ok(UserInfoOutputModel.fromDomain(response.value))
+				}
 
-                is Failure -> {
-                    Problem.UserNotFound.response(NOT_FOUND)
-                }
-            }
-        }
+				is Failure -> {
+					Problem.UserNotFound.response(NOT_FOUND)
+				}
+			}
+		}
 
-        @PutMapping(CHANNEL_ID_USER_ID_URL)
-        fun joinChannel(
-            @PathVariable channelId: UInt,
-            @PathVariable userId: UInt,
-            @RequestParam invitationCode: String = "",
-        ) {
-            when (val response = userService.joinChannel(userId, channelId, invitationCode)) {
-                is Success -> {
-                    ResponseEntity.ok()
-                }
+		@PutMapping(CHANNEL_ID_USER_ID_URL)
+		fun joinChannel(
+			@PathVariable channelId: UInt,
+			@PathVariable userId: UInt,
+			@RequestParam invitationCode: String = "",
+		) {
+			when (val response = userService.joinChannel(userId, channelId, invitationCode)) {
+				is Success -> {
+					ResponseEntity.ok()
+				}
 
-                is Failure -> {
-                    when (response.value) {
-                        UserNotFound -> Problem.UserNotFound.response(NOT_FOUND)
-                        ChannelNotFound -> ChannelProblem.ChannelNotFound.response(NOT_FOUND)
-                        else -> Problem.UnableToJoinChannel.response(BAD_REQUEST)
-                    }
-                }
-            }
-        }
+				is Failure -> {
+					when (response.value) {
+						UserNotFound -> Problem.UserNotFound.response(NOT_FOUND)
+						ChannelNotFound -> ChannelProblem.ChannelNotFound.response(NOT_FOUND)
+						InvitationCodeIsInvalid -> Problem.InvitationCodeIsInvalid.response(BAD_REQUEST)
+						InvitationCodeHasExpired -> Problem.InvitationCodeHasExpired.response(BAD_REQUEST)
+						InvitationCodeMaxUsesReached -> Problem.InvitationCodeMaxUsesReached.response(BAD_REQUEST)
+						else -> Problem.UnableToJoinChannel.response(BAD_REQUEST)
+					}
+				}
+			}
+		}
 
-        companion object {
-            /**
-             * The base URL for the user endpoints.
-             */
-            const val USER_BASE_URL = "/users"
+		private fun setCookie(
+			token: String,
+			response: HttpServletResponse,
+		) {
+			val cookie = Cookie("session", token)
+			cookie.isHttpOnly = true
+			cookie.secure = true
+			cookie.maxAge = 60 * 60 * 24
+			response.addCookie(cookie)
+		}
 
-            /**
-             * The URL for the user with the given id.
-             */
-            const val USER_ID_URL = "$USER_BASE_URL/{userId}"
+		companion object {
+			/**
+			 * The base URL for the user endpoints.
+			 */
+			const val USER_BASE_URL = "/users"
 
-            /**
-             * The URL for the user with the given id, the channel with the given id and invitation code.
-             */
-            const val CHANNEL_ID_USER_ID_URL = "${ChannelController.CHANNEL_ID_URL}$USER_ID_URL"
-        }
-    }
+			/**
+			 * The URL for the user with the given id.
+			 */
+			const val USER_ID_URL = "$USER_BASE_URL/{userId}"
+
+			/**
+			 * The URL for the user with the given id, the channel with the given id and invitation code.
+			 */
+			const val CHANNEL_ID_USER_ID_URL = "${ChannelController.CHANNEL_ID_URL}$USER_ID_URL"
+		}
+	}
