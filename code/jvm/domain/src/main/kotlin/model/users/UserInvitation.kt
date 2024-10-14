3c9180185c3a619160a53acdb7aab61d1d6e929a
Brgm37 <brgm37@gmail.com>
2024-10-14 23:24:02 +0100

    UserInvitation now receives a Timestamp instead of a LocalDateTime

--- code/jvm/domain/src/main/kotlin/model/users/UserInvitation.kt
+++ code/jvm/domain/src/main/kotlin/model/users/UserInvitation.kt
@@ -1,5 +1,6 @@
 package model.users
 
+import java.sql.Timestamp
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -10,12 +11,12 @@ import java.util.UUID
  * @param invitationCode The invitation code to join the app.
  */
 data class UserInvitation(
-    val userId: UInt,
-    val expirationDate: LocalDateTime,
-    val invitationCode: UUID = UUID.randomUUID(),
+	val userId: UInt,
+	val expirationDate: Timestamp,
+	val invitationCode: UUID = UUID.randomUUID(),
 ) {
-    /**
-     * Checks if the invitation is expired.
-     */
-    val isExpired: Boolean get() = expirationDate.isBefore(LocalDateTime.now())
+	/**
+	 * Checks if the invitation is expired.
+	 */
+	val isExpired: Boolean get() = expirationDate < Timestamp.valueOf(LocalDateTime.now())
 }
