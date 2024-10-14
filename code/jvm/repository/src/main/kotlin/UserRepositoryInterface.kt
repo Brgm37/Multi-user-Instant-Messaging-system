cd6676a934f412a1b4fdac6fb6f08d548c4e6e27
Brgm37 <brgm37@gmail.com>
2024-10-14 23:31:43 +0100

    UserJDBC missing functions implementations were added and some bugs were corrected.

--- code/jvm/repository/src/main/kotlin/UserRepositoryInterface.kt
+++ code/jvm/repository/src/main/kotlin/UserRepositoryInterface.kt
@@ -2,39 +2,39 @@ import model.users.User
 import model.users.UserInvitation
 
 interface UserRepositoryInterface : Repository<User> {
-    /**
-     * Creates a new user and retrieves its ID
-     * @param user The user to create
-     * @return The created user
-     */
-    fun createUser(user: User): User?
+	/**
+	 * Creates a new user and retrieves its ID
+	 * @param user The user to create
+	 * @return The created user
+	 */
+	fun createUser(user: User): User?
 
-    /**
-     * Retrieves an invitation associated to the user
-     * @param inviterUId The ID of the inviter
-     * @param invitationCode The invitation code
-     */
-    fun findInvitation(
-        inviterUId: UInt,
-        invitationCode: String,
-    ): UserInvitation?
+	/**
+	 * Retrieves an invitation associated to the user
+	 * @param inviterUId The ID of the inviter
+	 * @param invitationCode The invitation code
+	 */
+	fun findInvitation(
+		inviterUId: UInt,
+		invitationCode: String,
+	): UserInvitation?
 
-    /**
-     * Deletes an invitation associated to the user
-     * @param invitation The invitation to delete
-     */
-    fun deleteInvitation(invitation: UserInvitation)
+	/**
+	 * Deletes an invitation associated to the user
+	 * @param invitation The invitation to delete
+	 */
+	fun deleteInvitation(invitation: UserInvitation)
 
-    /**
-     * Creates an invitation for the user
-     * @param invitation The invitation to create
-     */
-    fun createInvitation(invitation: UserInvitation)
+	/**
+	 * Creates an invitation for the user
+	 * @param invitation The invitation to create
+	 */
+	fun createInvitation(invitation: UserInvitation)
 
-    /**
-     * Validates a token
-     *
-     * @param token The token to validate
-     */
-    fun validateToken(token: String): Boolean
+	/**
+	 * Validates a token
+	 *
+	 * @param token The token to validate
+	 */
+	fun validateToken(token: String): Boolean
 }
--- code/jvm/repository_jdbc/src/main/kotlin/jdbc/UserJDBC.kt
+++ code/jvm/repository_jdbc/src/main/kotlin/jdbc/UserJDBC.kt
@@ -9,142 +9,195 @@ import java.sql.ResultSet
 import java.util.UUID
 
 class UserJDBC(
-    private val connection: Connection,
+	private val connection: Connection,
 ) : UserRepositoryInterface {
-    private fun ResultSet.toUser(): User =
-        User(
-            uId = getInt("id").toUInt(),
-            username = getString("name"),
-            password = Password(getString(("password"))),
-            token = UUID.fromString(getString("token")),
-        )
+	private fun ResultSet.toUser(): User =
+		User(
+			uId = getInt("id").toUInt(),
+			username = getString("name"),
+			password = Password(getString(("password"))),
+			token = UUID.fromString(getString("token")),
+		)
 
-    override fun createUser(user: User): User? {
-        val insertQuery =
-            """
-            INSERT INTO users (name, password, token)
-            VALUES (?, ?, ?) RETURNING id
-            """.trimIndent()
-        val stm = connection.prepareStatement(insertQuery)
-        var idx = 1
-        stm.setString(idx++, user.username)
-        stm.setString(idx++, (user.password.value))
-        stm.setString(idx, user.token.toString())
-        val rs = stm.executeQuery()
-        return if (rs.next()) {
-            user.copy(uId = rs.getInt("id").toUInt())
-        } else {
-            null
-        }
-    }
+	private fun ResultSet.toUserInvitation(): UserInvitation =
+		UserInvitation(
+			userId = getInt("user_id").toUInt(),
+			invitationCode = UUID.fromString(getString("invitation")),
+			expirationDate = getTimestamp("expirationDate"),
+		)
 
-    override fun findInvitation(
-        inviterUId: UInt,
-        invitationCode: String,
-    ): UserInvitation? {
-        TODO("Not yet implemented")
-    }
+	override fun createUser(user: User): User? {
+		val insertQuery =
+			"""
+			INSERT INTO users (name, password, token)
+			VALUES (?, ?, ?) RETURNING id
+			""".trimIndent()
+		val stm = connection.prepareStatement(insertQuery)
+		var idx = 1
+		stm.setString(idx++, user.username)
+		stm.setString(idx++, (user.password.value))
+		stm.setString(idx, user.token.toString())
+		val rs = stm.executeQuery()
+		return if (rs.next()) {
+			user.copy(uId = rs.getInt("id").toUInt())
+		} else {
+			null
+		}
+	}
 
-    override fun deleteInvitation(invitation: UserInvitation) {
-        TODO("Not yet implemented")
-    }
+	override fun findInvitation(
+		inviterUId: UInt,
+		invitationCode: String,
+	): UserInvitation? {
+		val selectQuery =
+			"""
+			SELECT user_id, invitation, expirationDate
+			FROM users_invitations
+			WHERE user_id = ? AND invitation = ?
+			""".trimIndent()
+		val stm = connection.prepareStatement(selectQuery)
+		stm.setInt(1, inviterUId.toInt())
+		stm.setString(2, invitationCode)
+		val rs = stm.executeQuery()
+		return if (rs.next()) {
+			rs.toUserInvitation()
+		} else {
+			null
+		}
+	}
 
-    override fun createInvitation(invitation: UserInvitation) {
-        TODO("Not yet implemented")
-    }
+	override fun deleteInvitation(invitation: UserInvitation) {
+		val deleteQuery =
+			"""
+			DELETE FROM users_invitations
+			WHERE user_id = ? AND invitation = ?
+			""".trimIndent()
+		val stm = connection.prepareStatement(deleteQuery)
+		stm.setInt(1, invitation.userId.toInt())
+		stm.setString(2, invitation.invitationCode.toString())
+		stm.executeUpdate()
+	}
 
-    override fun validateToken(token: String): Boolean {
-        val selectQuery =
-            """
-            SELECT id
-            FROM users
-            WHERE token = ?
-            """.trimIndent()
-        val stm = connection.prepareStatement(selectQuery)
-        stm.setString(1, token)
-        val rs = stm.executeQuery()
-        return rs.next()
-    }
+	override fun createInvitation(invitation: UserInvitation) {
+		val insertQuery =
+			"""
+			INSERT INTO users_invitations (user_id, invitation, expirationDate)
+			VALUES (?, ?, ?)
+			""".trimIndent()
+		val stm = connection.prepareStatement(insertQuery)
+		stm.setInt(1, invitation.userId.toInt())
+		stm.setString(2, invitation.invitationCode.toString())
+		stm.setTimestamp(3, invitation.expirationDate)
+		stm.executeUpdate()
+	}
 
-    override fun findById(id: UInt): User? {
-        val selectQuery =
-            """
-            SELECT id, name, password, token
-            FROM users
-            WHERE id = ?
-            """.trimIndent()
-        val stm = connection.prepareStatement(selectQuery)
-        stm.setInt(1, id.toInt())
-        val rs = stm.executeQuery()
-        return if (rs.next()) {
-            rs.toUser()
-        } else {
-            null
-        }
-    }
+	override fun validateToken(token: String): Boolean {
+		val selectQuery =
+			"""
+			SELECT id
+			FROM users
+			WHERE token = ?
+			""".trimIndent()
+		val stm = connection.prepareStatement(selectQuery)
+		stm.setString(1, token)
+		val rs = stm.executeQuery()
+		return rs.next()
+	}
 
-    override fun findAll(
-        offset: Int,
-        limit: Int,
-    ): List<User> {
-        val selectQuery =
-            """
-            SELECT id, name, password, token
-            FROM users
-            """.trimIndent()
-        val stm = connection.prepareStatement(selectQuery)
-        val rs = stm.executeQuery()
-        val users = mutableListOf<User>()
-        while (rs.next()) {
-            users.add(rs.toUser())
-        }
-        return users
-    }
+	override fun findById(id: UInt): User? {
+		val selectQuery =
+			"""
+			SELECT id, name, password, token
+			FROM users
+			WHERE id = ?
+			""".trimIndent()
+		val stm = connection.prepareStatement(selectQuery)
+		stm.setInt(1, id.toInt())
+		val rs = stm.executeQuery()
+		return if (rs.next()) {
+			rs.toUser()
+		} else {
+			null
+		}
+	}
 
-    override fun save(entity: User) {
-        val updateQuery =
-            """
-            UPDATE users
-            SET name = ?, password = ?
-            WHERE id = ?
-            """.trimIndent()
-        val stm = connection.prepareStatement(updateQuery)
-        var idx = 1
-        stm.setString(idx++, entity.username)
-        stm.setString(idx++, entity.password.value)
-        entity.uId?.let { stm.setInt(idx, it.toInt()) }
-        stm.executeUpdate()
-    }
+	override fun findAll(
+		offset: Int,
+		limit: Int,
+	): List<User> {
+		val selectQuery =
+			"""
+			SELECT id, name, password, token
+			FROM users
+			""".trimIndent()
+		val stm = connection.prepareStatement(selectQuery)
+		val rs = stm.executeQuery()
+		val users = mutableListOf<User>()
+		while (rs.next()) {
+			users.add(rs.toUser())
+		}
+		return users
+	}
 
-    override fun deleteById(id: UInt) {
-        val deleteFromUserChannelsQuery =
-            """
-            DELETE FROM channel_members
-            WHERE member = ?
-            """.trimIndent()
-        val deleteFromUsersQuery =
-            """
-            DELETE FROM users
-            WHERE id = ?
-            """.trimIndent()
+	override fun save(entity: User) {
+		val updateQuery =
+			"""
+			UPDATE users
+			SET name = ?, password = ?
+			WHERE id = ?
+			""".trimIndent()
+		val stm = connection.prepareStatement(updateQuery)
+		var idx = 1
+		stm.setString(idx++, entity.username)
+		stm.setString(idx++, entity.password.value)
+		entity.uId?.let { stm.setInt(idx, it.toInt()) }
+		stm.executeUpdate()
+	}
 
-        val stmUserChannels = connection.prepareStatement(deleteFromUserChannelsQuery)
-        stmUserChannels.setInt(1, id.toInt())
-        stmUserChannels.executeUpdate()
+	override fun deleteById(id: UInt) {
+		val deleteFromUserChannelsQuery =
+			"""
+			DELETE FROM channel_members
+			WHERE member = ?
+			""".trimIndent()
+		val deleteFromUsersQuery =
+			"""
+			DELETE FROM users
+			WHERE id = ?
+			""".trimIndent()
 
-        val stmUsers = connection.prepareStatement(deleteFromUsersQuery)
-        stmUsers.setInt(1, id.toInt())
-        stmUsers.executeUpdate()
-    }
+		val stmUserChannels = connection.prepareStatement(deleteFromUserChannelsQuery)
+		stmUserChannels.setInt(1, id.toInt())
+		stmUserChannels.executeUpdate()
 
-    @Suppress("SqlWithoutWhere")
-    override fun clear() {
-        val deleteFromUsersQuery =
-            """
-            DELETE FROM users
-            """.trimIndent()
+		val stmUsers = connection.prepareStatement(deleteFromUsersQuery)
+		stmUsers.setInt(1, id.toInt())
+		stmUsers.executeUpdate()
+	}
 
-        val stmUsers = connection.prepareStatement(deleteFromUsersQuery)
-        stmUsers.executeUpdate()
-    }
+	@Suppress("SqlWithoutWhere")
+	override fun clear() {
+		val deleteFromUsersQuery =
+			"""
+			DELETE FROM users
+			""".trimIndent()
+		val deleteFromUsersInvitationsQuery =
+			"""
+			DELETE FROM users_invitations
+			""".trimIndent()
+		val insertQuery =
+			"""
+			INSERT INTO users (name, password, token)
+			VALUES ('TheChosenOne
+			', 'Password123', '0f7ed58e-89c0-4331-b22d-0d075b356317')
+			""".trimIndent()
+		val stmDeleteInvitations = connection.prepareStatement(deleteFromUsersInvitationsQuery)
+		stmDeleteInvitations.executeUpdate()
+
+		val stmDelete = connection.prepareStatement(deleteFromUsersQuery)
+		stmDelete.executeUpdate()
+
+		val stmInsert = connection.prepareStatement(insertQuery)
+		stmInsert.executeUpdate()
+	}
 }
