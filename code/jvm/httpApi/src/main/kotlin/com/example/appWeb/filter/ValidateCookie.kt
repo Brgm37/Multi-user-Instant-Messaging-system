034e4d3a534768a2337b193ddaa4cbf39ac304d6
Brgm37 <brgm37@gmail.com>
2024-10-14 23:34:48 +0100

    merge commit.

--- code/jvm/httpApi/src/main/kotlin/com/example/appWeb/filter/ValidateCookie.kt
+++ code/jvm/httpApi/src/main/kotlin/com/example/appWeb/filter/ValidateCookie.kt
@@ -6,40 +6,41 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpFilter
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 import utils.Failure
 import utils.Success
 
 class ValidateCookie(
-    private val service: UserServicesInterface,
+	private val service: UserServicesInterface,
 ) : HttpFilter() {
-    override fun doFilter(
-        request: ServletRequest,
-        response: ServletResponse,
-        chain: FilterChain,
-    ) {
-        val httpRequest = request as jakarta.servlet.http.HttpServletRequest
+	override fun doFilter(
+		request: ServletRequest,
+		response: ServletResponse,
+		chain: FilterChain,
+	) {
+		val httpRequest = request as HttpServletRequest
 //            val httpResponse = response as jakarta.servlet.http.HttpServletResponse
-        val cookie = httpRequest.cookies?.find { it.name == "session" }
-        if (cookie == null) {
+		val cookie = httpRequest.cookies?.find { it.name == "session" }
+		if (cookie == null) {
 //                httpResponse.sendRedirect("/login") // TODO: confirm if this is the correct redirect
-            Problem.Unauthorized.response(UNAUTHORIZED)
-            return
-        }
-        when (val isValid = service.isValidToken(cookie.value)) {
-            is Success -> {
-                if (isValid.value) {
-                    chain.doFilter(request, response)
-                } else {
+			Problem.Unauthorized.response(UNAUTHORIZED)
+			return
+		}
+		when (val isValid = service.isValidToken(cookie.value)) {
+			is Success -> {
+				if (isValid.value) {
+					chain.doFilter(request, response)
+				} else {
 //                        httpResponse.sendRedirect("/login") // TODO: confirm if this is the correct redirect
-                    Problem.Unauthorized.response(UNAUTHORIZED)
-                }
-            }
+					Problem.Unauthorized.response(UNAUTHORIZED)
+				}
+			}
 
-            is Failure -> {
+			is Failure -> {
 //                    httpResponse.sendRedirect("/login") // TODO: confirm if this is the correct redirect
-                Problem.Unauthorized.response(UNAUTHORIZED)
-            }
-        }
-    }
+				Problem.Unauthorized.response(UNAUTHORIZED)
+			}
+		}
+	}
 }
