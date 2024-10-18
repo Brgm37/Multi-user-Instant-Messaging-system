0fa22550d4f2710c3eb5e143855ebbbad121ea9e
mariana-moraes <A50560@alunos.isel.pt>
2024-10-18 22:34:04 +0100

    Message controller ktlint + minor alterations

--- code/jvm/httpApi/src/main/kotlin/com/example/appWeb/controller/MessageController.kt
+++ code/jvm/httpApi/src/main/kotlin/com/example/appWeb/controller/MessageController.kt
@@ -2,9 +2,8 @@ package com.example.appWeb.controller
 
 import com.example.appWeb.controller.ChannelController.Companion.CHANNEL_BASE_URL
 import com.example.appWeb.controller.ChannelController.Companion.CHANNEL_ID_URL
-import com.example.appWeb.controller.UserController.Companion.USER_BASE_URL
-import com.example.appWeb.controller.UserController.Companion.USER_ID_URL
 import com.example.appWeb.model.dto.input.message.CreateMessageInputModel
+import com.example.appWeb.model.dto.input.user.AuthenticatedUserInputModel
 import com.example.appWeb.model.dto.output.message.MessageOutputModel
 import com.example.appWeb.model.problem.ChannelProblem
 import com.example.appWeb.model.problem.MessageProblem
@@ -25,28 +24,27 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import utils.Failure
 import utils.Success
-import java.sql.Timestamp
-import java.time.LocalDateTime
 
 @Controller
 class MessageController(
     private val messageService: MessageServicesInterface,
 ) {
-        @PostMapping(MESSAGE_CREATE_URL)
-        fun createMessage(
-            @Valid @RequestBody message: CreateMessageInputModel,
-        ) {
-            val response =
-                messageService
-                    .createMessage(
-                        msg = message.msg,
-                        user = message.user,
-                        channel = message.channel,
-                    )
-            when (response) {
-                is Success -> {
-                    ResponseEntity.ok(MessageOutputModel.fromDomain(response.value))
-                }
+    @PostMapping(MESSAGE_CREATE_URL)
+    fun createMessage(
+        @Valid @RequestBody message: CreateMessageInputModel,
+        authenticated: AuthenticatedUserInputModel,
+    ): ResponseEntity<*> {
+        val response =
+            messageService
+                .createMessage(
+                    msg = message.msg,
+                    user = authenticated.uId,
+                    channel = message.channel,
+                )
+        return when (response) {
+            is Success -> {
+                ResponseEntity.ok(MessageOutputModel.fromDomain(response.value))
+            }
 
             is Failure -> {
                 when (response.value) {
@@ -59,43 +57,41 @@ class MessageController(
         }
     }
 
-        @GetMapping(MESSAGE_ID_URL)
-        fun getSingleMessage(
-            @PathVariable userId: UInt,
-            @PathVariable msgId: UInt,
-        ) {
-            when (val response = messageService.getMessage(msgId, userId)) {
-                is Success -> {
-                    ResponseEntity.ok(MessageOutputModel.fromDomain(response.value))
-                }
+    @GetMapping(MESSAGE_ID_URL)
+    fun getSingleMessage(
+        @PathVariable msgId: UInt,
+        authenticated: AuthenticatedUserInputModel,
+    ): ResponseEntity<*> =
+        when (val response = messageService.getMessage(msgId, authenticated.uId)) {
+            is Success -> {
+                ResponseEntity.ok(MessageOutputModel.fromDomain(response.value))
+            }
 
-                is Failure -> {
-                    MessageProblem.MessageNotFound.response(NOT_FOUND)
-                }
+            is Failure -> {
+                MessageProblem.MessageNotFound.response(NOT_FOUND)
             }
         }
 
-        @GetMapping(CHANNEL_MESSAGES_URL)
-        fun getChannelMessages(
-            @PathVariable userId: UInt,
-            @PathVariable channelId: UInt,
-            @RequestParam offset: Int,
-            @RequestParam limit: Int,
-        ) {
-            when (val response = messageService.latestMessages(channelId, userId, offset, limit)) {
-                is Success -> {
-                    ResponseEntity.ok(response.value.map(MessageOutputModel::fromDomain))
-                }
+    @GetMapping(CHANNEL_MESSAGES_URL)
+    fun getChannelMessages(
+        @PathVariable channelId: UInt,
+        @RequestParam offset: Int,
+        @RequestParam limit: Int,
+        authenticated: AuthenticatedUserInputModel,
+    ): ResponseEntity<*> =
+        when (val response = messageService.latestMessages(channelId, authenticated.uId, offset, limit)) {
+            is Success -> {
+                ResponseEntity.ok(response.value.map(MessageOutputModel::fromDomain))
+            }
 
-                is Failure -> {
-                    MessageProblem.MessageNotFound.response(NOT_FOUND)
-                }
+            is Failure -> {
+                MessageProblem.MessageNotFound.response(NOT_FOUND)
             }
         }
 
-        companion object {
-            const val MESSAGE_CREATE_URL = "$USER_BASE_URL/$CHANNEL_BASE_URL/messages"
-            const val MESSAGE_ID_URL = "$USER_ID_URL/$CHANNEL_BASE_URL/messages/{msgId}"
-            const val CHANNEL_MESSAGES_URL = "$USER_ID_URL/$CHANNEL_ID_URL/messages"
-        }
+    companion object {
+        const val MESSAGE_CREATE_URL = "$CHANNEL_BASE_URL/messages"
+        const val MESSAGE_ID_URL = "$CHANNEL_BASE_URL/messages/{msgId}"
+        const val CHANNEL_MESSAGES_URL = "$CHANNEL_ID_URL/messages"
     }
+}
