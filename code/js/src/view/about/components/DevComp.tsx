c88162c65d87c8e46909acb0e6d9b0fdb44dbb5d
mariana-moraes <A50560@alunos.isel.pt>
2024-11-26 18:54:50 +0000

    start of about implementation

---
+++ code/js/src/view/about/components/DevComp.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import {Link} from "react-router-dom";
+
+export function AboutView(): React.JSX.Element  {
+    return (
+    <div>
+        <h1>{}<h1/>
+    </div>
+    )    
+}
