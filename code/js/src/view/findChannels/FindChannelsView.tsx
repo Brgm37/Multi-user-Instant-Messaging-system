a80a31dad466c5b9a706fbdd2867247e988354c5
Brgm37 <brgm37@gmail.com>
2024-11-28 19:44:35 +0000

    FindChannels hook first implementation

--- code/js/src/view/findChannels/FindChannelsView.tsx
+++ code/js/src/view/findChannels/FindChannelsView.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
 import { ChangeEvent, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { PublicChannelsContext } from "./ChannelsProvider";
-import { reduce } from "../../service/findChannels/FindChannelsReducer";
+import { reduce } from "./hooks/FindChannelsReducer";
 
 const DEBOUNCE_DELAY = 500;
 
-export function FindChannels(): React.JSX.Element {
-    const { channels, setChannels } = React.useContext(PublicChannelsContext);
-    const [state, dispatch] = React.useReducer(reduce, {
-        tag: "navigating",
-        searchBar: "",
-        channels: []
-    });
-
-    
-}
+// export function FindChannels(): React.JSX.Element {
+//     const [state, dispatch] = React.useReducer(reduce, {
+//         tag: "navigating",
+//         searchBar: "",
+//         channels: []
+//     });
+//
+//
--- code/js/src/view/findChannels/hooks/FindChannelsReducer.ts
+++ code/js/src/view/findChannels/hooks/FindChannelsReducer.ts
@@ -1,6 +1,29 @@
 import {Action} from "./states/FindChannelsAction";
 import {State} from "./states/FindChannelsState";
+import {useEffect, useReducer} from "react";
+import {useFetch} from "../../../service/utils/useFetch";
+import {urlBuilder} from "../../../service/utils/UrlBuilder";
+import {channelsToPublicChannels} from "../model/findChannels/PublicChannel";
 
+/**
+ * The URL for the find channels API.
+ */
+const findChannelsApiUrl = urlBuilder("/channels/search")
+
+/**
+ * The delay for debounce.
+ *
+ */
+const DEBOUNCE_DELAY = 500;
+
+/**
+ * The reducer function for the find channels form.
+ *
+ * @param state
+ * @param action
+ *
+ * @returns State
+ */
 export function reduce(state: State, action: Action): State {
     switch (state.tag) {
         case "navigating":
@@ -41,3 +64,98 @@ export function reduce(state: State, action: Action): State {
         }
     }
 }
+
+export type UseFindChannelsHandler = {
+    /**
+     * The function to call when the search bar changes.
+     * @param searchBar
+     * @returns void
+     */
+    onSearchChange: (searchBar: string) => void
+
+    /**
+     * The function to call when the user wants to fetch more channels.
+     *
+     * @returns void
+     */
+    onFetch: () => void
+
+    /**
+     * The function to call when the user wants to join a channel.
+     *
+     * @param channelId The id of the channel to join.
+     * @returns void
+     */
+    onJoin: (channelId: string) => void
+
+    /**
+     * The function to call when the user wants to close the error message.
+     *
+     * @returns void
+     */
+    onErrorClose: () => void
+
+}
+
+const initialState: State = { tag: "navigating", searchBar: "", channels: [] }
+
+export function useFindChannels() {
+    const [state, dispatch] = useReducer(reduce, initialState)
+
+    useEffect(() => {
+        if (state.tag != "navigating") return
+        const timeout = setTimeout(() => {
+            dispatch({ type: "search", channels: [] })
+        }, DEBOUNCE_DELAY)
+        return () => clearTimeout(timeout)
+    }, [state]
+    )
+
+    const onSearchChange = () => {
+        if (state.tag != "navigating") return
+        useFetch(
+            findChannelsApiUrl + '/' + state.searchBar,
+            "GET",
+            response => response.json().then((channels) =>
+                dispatch({ type: "success", channels: channelsToPublicChannels(channels) })),
+            response => dispatch({ type: "error", error: response.message }),
+            {}
+        ).toFetch()
+        dispatch({ type: "search", channels: [] })
+    }
+
+    const onFetch = () => {
+        dispatch({ type: "fetchMore", channels: [] })
+    }
+
+    const onErrorClose = () => {
+        dispatch({ type: "closeError" })
+    }
+
+    const onJoin = (channelId: number) => {
+        dispatch({ type: "join", channelId: channelId })
+    }
+
+    return [ state, {onSearchChange} ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
