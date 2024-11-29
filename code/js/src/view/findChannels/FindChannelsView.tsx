aa97ef9450b08c138e86234c72a9bc095a85f8e8
Brgm37 <brgm37@gmail.com>
2024-11-29 17:38:36 +0000

    FindChannelsView now receives a FindChannelService SearchBar component implementation

--- code/js/src/view/findChannels/FindChannelsView.tsx
+++ code/js/src/view/findChannels/FindChannelsView.tsx
@@ -1,17 +1,45 @@
 import * as React from 'react';
-import { reduce } from "./hooks/UseFindChannels";
+import {reduce, useFindChannels} from "./hooks/UseFindChannels";
 import {FindChannelsService, makeDefaultFindChannelService} from "../../service/findChannels/FindChannelService";
+import {useLocation} from "react-router-dom";
+import {State} from "./hooks/states/FindChannelsState";
+import {SearchBar} from "./components/shared/SearchBar";
 
 const DEBOUNCE_DELAY = 500;
 
 export function FindChannels(
     service: FindChannelsService = makeDefaultFindChannelService()
 ): React.JSX.Element {
-    const [state, dispatch] = React.useReducer(reduce, {
-        tag: "navigating",
-        searchBar: "",
-        channels: []
+    const location = useLocation();
+    const [state, handler] = useFindChannels()
+
+    const view = ((state: State) => {
+        switch (state.tag) {
+            case "navigating":
+                return <FindChannelsView handler={handler}/>
+            case "searching":
+                return <FindChannelsSearchingView handler={handler}/>
+            case "fetchingMore":
+                return <FindChannelsFetchingMoreView handler={handler}/>
+            case "error":
+                return <FindChannelsErrorView handler={handler}/>
+            case "joining":
+                return <FindChannelsJoiningView handler={handler}/>
+        }
     })
+
+    return (
+        <div>
+            <h1>Find Channels</h1>
+            <SearchBar value={} onChange={}
+            {view(state)}
+        </div>
+    )
+
+}
+
+
+
 
 
 
---
+++ code/js/src/view/findChannels/components/shared/SearchBar.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+/**
+ * The props for the search bar component.
+ */
+type SearchBarProps = {
+    value: string;
+    onChange: (value: string) => void;
+};
+
+/**
+ * A search bar component.
+ * @param value The value of the search bar.
+ * @param onChange The function to call when the search bar changes.
+ * @returns JSX.Element
+ */
+export function SearchBar(
+    {value, onChange}: SearchBarProps
+): React.JSX.Element {
+    return (
+        <div>
+            <input
+                type="text"
+                value={value}
+                onChange={(event) => onChange(event.target.value)}/>
+                placeholder="Search for channels"
+        </div>
+    )
+}
