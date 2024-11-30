3ed378c0d38ad80155e6c098fec058bf12da5404
Brgm37 <brgm37@gmail.com>
2024-11-30 16:01:46 +0000

    FindChannelErrorView now shows the error that caused it and has a close button

--- code/js/src/view/findChannels/components/FindChannelsErrorView.tsx
+++ code/js/src/view/findChannels/components/FindChannelsErrorView.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 export function FindChannelsErrorView(
-    {error}:  {error: string}
+    {error, onClose}:  {error: string | Error, onClose: () => void}
 ): React.JSX.Element {
+    const errorMessage = typeof error === "string" ? error : error.message;
     return (
         <div>
             <h1>FindChannelsErrorView</h1>
-            <div>{error}</div>
+            <div>{errorMessage}</div>
+            <button onClick={onClose}>Close</button>
         </div>
     )
 }
