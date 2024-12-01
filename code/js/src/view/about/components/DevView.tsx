4929348fd8d417ff1f4c4c7a78bc578a0b764d49
mariana-moraes <A50560@alunos.isel.pt>
2024-12-01 23:57:30 +0000

    DevView Alterations

--- code/js/src/view/about/components/DevView.tsx
+++ code/js/src/view/about/components/DevView.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react'
-import {Link} from "react-router-dom";
-import { Dev } from '../../../service/about/AboutServices';
+import {Dev} from "../hooks/states/AboutState";
 
-const dev: Dev = {
-    name: "John Doe",
-    bio: "I am a developer",
-    email: "johndoe@gmail.com",
-    github: new URL("https://github.com"),
-    linkdIn: new URL("https://www.linkedin.com")
-}
 
-export function DevView(): React.JSX.Element  {
+export function DevView({ dev }: { dev: Dev }): React.JSX.Element  {
+    const [showDevInfo, setShowDevInfo] = React.useState(false)
+    const handleToggleDevInfo = () => setShowDevInfo(!showDevInfo)
     return (
         <div>
-            <h1>{dev.name}</h1>
-            <p>{dev.bio}</p>
-            <p>{dev.email}</p>
-            <a href={dev.github.href}>Github</a>
+            <button onClick={handleToggleDevInfo}>{dev.name}</button>
+            {showDevInfo ? (
+                <>
+                    <p>{dev.num}</p>
+                    <p>{dev.bio || null}</p>
+                    <p>{dev.email}</p>
+                    <a href={dev.github.href}>Github</a>
+                    <p></p>
+                    <a href={dev.linkdIn.href}>LinkedIn</a>
+                </>
+            ) : null}
             <p></p>
-            <a href={dev.linkdIn.href}>LinkedIn</a>
         </div>
     )
 }
