0747d122640efa8e7121fd18064d2707c49de6a7
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-12-02 16:03:14 +0000

    useScroll logic was implemented in useChannels.

--- code/js/src/view/channels/hooks/UseChannels.ts
+++ code/js/src/view/channels/hooks/UseChannels.ts
@@ -1,22 +1,30 @@
-import * as React from "react";
 import {ChannelsState} from "./state/ChannelsState";
 import {ChannelsAction} from "./state/ChannelsAction";
-import {useReducer} from "react";
+import {useContext, useEffect, useReducer} from "react";
 import {ChannelHandler} from "./handler/ChannelHandler";
+import {ChannelsServiceContext} from "../../../service/channels/ChannelsServiceContext";
+import useScroll, {HasMore, UseScrollHandler, UseScrollState} from "../../../service/utils/hooks/useScroll/UseScroll";
+import {Channel} from "../../../model/Channel";
+import envConfig from "../../../../envConfig.json"
+
+const LIST_LIMIT = envConfig.list_limit
+const DEFAULT_LIMIT = envConfig.default_limit
+let limit = DEFAULT_LIMIT
+const HAS_MORE = DEFAULT_LIMIT + 1
 
 function reduce(state: ChannelsState, action: ChannelsAction): ChannelsState {
     switch (state.tag) {
         case "idle":
             switch (action.tag) {
                 case "init":
-                    return {tag: "loading", channels: []}
+                    return {tag: "loading", channels: state.channels, at: "both"}
                 default:
                     throw Error(`Action ${action.tag} is not allowed in state ${state.tag}`)
             }
         case "loading":
             switch (action.tag) {
                 case "loadSuccess":
-                    return {tag: "channels", channels: action.channels, hasMore: action.hasMore}
+                    return {...state, tag: "channels", channels: action.channels}
                 case "loadError":
                     return {tag: "error", message: action.message, previous: action.previous}
                 default:
@@ -25,29 +33,31 @@ function reduce(state: ChannelsState, action: ChannelsAction): ChannelsState {
         case "channels":
             switch (action.tag) {
                 case "search":
-                    return {tag: "searching", query: action.query}
+                    return {tag: "searching", query: action.query, channels: state.channels}
                 case "loadMore":
-                    return {tag: "loading", channels: state.channels}
+                    return {tag: "loading", channels: state.channels, at: action.at}
                 default:
                     throw Error(`Action ${action.tag} is not allowed in state ${state.tag}`)
             }
         case "searching":
             switch (action.tag) {
                 case "searchSuccess":
-                    return {tag: "searchResults", channels: action.channels, hasMore: action.hasMore}
+                    return {...state, tag: "searchResults", channels: action.channels}
                 case "searchError":
                     return {tag: "error", message: action.message, previous: state}
+                case "clear":
+                    return {tag: "loading", channels: state.channels, at: "both"}
                 default:
                     throw Error(`Action ${action.tag} is not allowed in state ${state.tag}`)
             }
         case "searchResults":
             switch (action.tag) {
                 case "loadMore":
-                    return {tag: "loading", channels: state.channels}
+                    return {tag: "loading", channels: state.channels, at: action.at}
                 case "search":
-                    return {tag: "searching", query: action.query}
+                    return {tag: "searching", query: action.query, channels: state.channels}
                 case "clear":
-                    return {tag: "channels", channels: state.channels, hasMore: state.hasMore}
+                    return {tag: "loading", channels: state.channels, at: "both"}
                 default:
                     throw Error(`Action ${action.tag} is not allowed in state ${state.tag}`)
             }
@@ -61,14 +71,99 @@ function reduce(state: ChannelsState, action: ChannelsAction): ChannelsState {
     }
 }
 
+function resetList(
+    listHandler: UseScrollHandler<Channel>,
+    result: Channel[],
+): void {
+    const channels = result.slice(0, limit)
+    listHandler.reset(
+        channels,
+        result.length === HAS_MORE ? {head: false, tail: true} : {head: false, tail: false}
+    )
+}
+
+function addItems(
+    at: "head" | "tail",
+    offset: number,
+    listHandler: UseScrollHandler<Channel>,
+    list: UseScrollState<Channel>,
+    result: Channel[],
+): void {
+    const channels = result.slice(0, limit)
+    if (channels.length < limit) limit = channels.length
+    else limit = DEFAULT_LIMIT
+    const tail =
+        result.length === HAS_MORE && at === "tail" ||
+        at === "head" && list.list.length === LIST_LIMIT
+    const hasMore: HasMore = {
+        head: offset > 0 && list.list.length === LIST_LIMIT,
+        tail
+    }
+    listHandler.addItems(channels, at, hasMore)
+}
+
 export default function (): [ChannelsState, ChannelHandler] {
-    const [state, dispatch] = useReducer(reduce, {tag: "idle"})
+    const [list, listHandler] = useScroll<Channel>(LIST_LIMIT)
+    const initialState: ChannelsState = {tag: "idle", channels: list}
+    const [state, dispatch] = useReducer(reduce, initialState)
+    const service = useContext(ChannelsServiceContext)
+    useEffect(() => {
+        if (state.tag === "error") return
+        if (state.tag === "searching") {
+            if (state.channels !== list) {
+                dispatch({tag: "searchSuccess", channels: list});
+            }
+        } else {
+            if (state.channels !== list) {
+                dispatch({tag: "loadSuccess", channels: list});
+            }
+        }
+    }, [list]);
     const handler: ChannelHandler = {
         clear(): void {
-        }, goBack(): void {
-        }, loadChannels(): void {
-        }, loadMore(): void {
-        }, search(query: string): void {
+            service.findChannels(0, HAS_MORE)
+                .then(result => {
+                    if (result.tag === "success") resetList(listHandler, result.value)
+                    else dispatch({tag: "loadError", message: result.value, previous: state})
+                })
+            dispatch({tag: "clear"})
+        },
+        goBack(): void {
+            dispatch({tag: "goBack"})
+        },
+        loadChannels(): void {
+            service.findChannels(0, HAS_MORE)
+                .then(result => {
+                    if (result.tag === "success") resetList(listHandler, result.value)
+                    else dispatch({tag: "loadError", message: result.value, previous: state})
+                })
+            dispatch({tag: "init"})
+        },
+        loadMore(offset, at): void {
+            if (state.tag === "loading") return
+            service.findChannels(offset * limit, HAS_MORE)
+                .then(result => {
+                    if (result.tag === "success") addItems(at, offset, listHandler, list, result.value)
+                    else dispatch({tag: "loadError", message: result.value, previous: state})
+                })
+            dispatch({tag: "loadMore", at})
+
+        },
+        search(query: string): void {
+            service.findChannelsByName(query, 0, HAS_MORE)
+                .then(result => {
+                    if (result.tag === "success") resetList(listHandler, result.value)
+                    else dispatch({tag: "searchError", message: result.value, previous: state})
+                })
+            dispatch({tag: "search", query})
+        },
+        searchMore(query: string, offset: number, at): void {
+            service.findChannelsByName(query, offset, HAS_MORE)
+                .then(result => {
+                    if (result.tag === "success") addItems(at, offset, listHandler, list, result.value)
+                    else dispatch({tag: "searchError", message: result.value, previous: state})
+                })
+            dispatch({tag: "loadMore", at})
         }
     }
     return [state, handler]
