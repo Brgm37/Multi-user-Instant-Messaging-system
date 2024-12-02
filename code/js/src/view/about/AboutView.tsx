bf29648d5b1ce3fed4b49f3b927dd909859ad49c
mariana-moraes <A50560@alunos.isel.pt>
2024-12-02 00:00:13 +0000

    AboutView Further Implementation

--- code/js/src/view/about/AboutView.tsx
+++ code/js/src/view/about/AboutView.tsx
@@ -1,13 +1,42 @@
 import * as React from 'react'
-import {Link} from "react-router-dom";
+import {DevView} from "./components/DevView";
+import {Dev} from "./hooks/states/AboutState";
 
 export function AboutView(): React.JSX.Element  {
     return (
         <div>
-            <h1>About</h1>
-            <p>This is the about page.</p>
-            <Link to="/">Home</Link>
+            <h1>About Developers</h1>
+            {Developers.map((dev) => (
+                <DevView dev={dev} />
+            ))}
         </div>
     )
-    
 }
+
+const Developers: Dev[] =
+    [
+        {
+            name: "Arthur Oliveira",
+            num: 50543,
+            bio: "I am a developer",
+            email: "A50543@alunos.isel.pt",
+            github: new URL("https://github.com/Thuzys"),
+            linkdIn: new URL("https://www.linkedin.com/in/arthur-cesar-oliveira-681643184/")
+        },
+        {
+            name: "Brian Melhorado",
+            num: 50471,
+            bio: null,
+            email: "A50471@alunos.isel.pt",
+            github: new URL("https://github.com/Brgm37"),
+            linkdIn: new URL("https://www.linkedin.com/in/brian-melhorado-449794307/")
+        },
+        {
+            name: "Mariana Moraes",
+            num: 50560,
+            bio: null,
+            email: "A50560@alunos.isel.pt",
+            github: new URL("https://github.com/mariana-moraess"),
+            linkdIn: new URL("https://www.linkedin.com/in/mariana-moraes-92975b330/")
+        }
+    ]
