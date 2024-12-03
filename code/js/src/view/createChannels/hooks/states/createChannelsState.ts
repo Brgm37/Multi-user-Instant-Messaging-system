a1bdc896919d92aff90146b8c75f03d452bbe9f4
mariana-moraes <A50560@alunos.isel.pt>
2024-12-03 11:46:03 +0000

    createChannelsState makeInicialState

--- code/js/src/view/createChannels/hooks/states/createChannelsState.ts
+++ code/js/src/view/createChannels/hooks/states/createChannelsState.ts
@@ -1,3 +1,5 @@
+import {useSearchParams} from "react-router-dom";
+import {extractSearchParams} from "../../../../service/utils/extractParams";
 
 type Visibility = "public" | "private"
 
@@ -12,6 +14,20 @@ type ChannelInput = {
 
 export type CreateChannelsState =
     { tag: "editing", input: ChannelInput, error?: string } |
-    { tag: "error", message: string } |
+    { tag: "error", message: string, input: ChannelInput } |
     { tag: "submitting", input: ChannelInput } |
     { tag: "redirecting" }
+
+
+export function makeInitialState(): CreateChannelsState {
+    const [searchParams] = useSearchParams()
+    return {
+        tag: "editing",
+        input: {
+            name: "",
+            visibility: "public",
+            access: "READ_ONLY",
+            isValid: false
+        }
+    }
+}
