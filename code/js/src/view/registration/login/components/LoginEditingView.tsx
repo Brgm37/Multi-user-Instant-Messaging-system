c2232ab831051f34060a4998e3cbfbed420b43a7
Brgm37 <brgm37@gmail.com>
2024-12-03 21:13:50 +0000

    LoginEditingView now implements LoginBaseView

--- code/js/src/view/registration/login/components/LoginEditingView.tsx
+++ code/js/src/view/registration/login/components/LoginEditingView.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import {InputLabel} from "../../components/InputLabel";
 import {InputLabelContext} from "../../components/InputLabelContext";
 import {UseLoginFormHandler} from "../hooks/handler/UseLoginFormHandler";
+import {Link} from "react-router-dom";
+import {LoginBaseView} from "./LoginBaseView";
 
 
 /**
@@ -11,7 +13,6 @@ import {UseLoginFormHandler} from "../hooks/handler/UseLoginFormHandler";
  */
 export function LoginEditingView({handler}: {handler: UseLoginFormHandler}): React.JSX.Element {
     const loginState = React.useContext(InputLabelContext)
-    const isVisible = loginState.visibility.password ? 'text' : 'password'
     const login = {
         value: loginState.input.username,
         onChange: (event: React.ChangeEvent<HTMLInputElement>) => handler.onUsernameChange(event.target.value),
@@ -23,11 +24,12 @@ export function LoginEditingView({handler}: {handler: UseLoginFormHandler}): Rea
         error: loginState.error.passwordError
     }
     return (
-        <div>
-            <InputLabel label="Username" type="text" disabled={false} input={login}/>
-            <InputLabel label="Password" type={isVisible} disabled={false} input={password}/>
-            <button onClick={handler.onSubmit} disabled={!loginState.input.isValid}>Submit</button>
-            <button onClick={handler.togglePasswordVisibility}>Toggle Password Visibility</button>
-        </div>
+        <LoginBaseView
+            login={login}
+            password={password}
+            loginState={loginState}
+            onSubmit={handler.onSubmit}
+            inputsDisabled={false}
+        />
     )
 }
