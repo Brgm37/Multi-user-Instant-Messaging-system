a7ae7a6162a9e22bdf8c9118ffdbcaefecbf006d
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-12-03 17:46:36 +0000

    router update.

--- code/js/src/router/router.tsx
+++ code/js/src/router/router.tsx
@@ -27,6 +27,7 @@ import {
     CreateChannelInvitationMockServiceProvider
 } from "../service/createChannelInvitation/mock/CreateChannelInvitationMockServiceProvider";
 import {CreateChannelInvitationView} from "../view/createChannelInvitation/CreateChannelInvitation";
+import {SseCommunicationServiceProvider} from "../service/sse/SseCommunicationServiceProvider";
 
 const router = createBrowserRouter(
     [
@@ -60,9 +61,11 @@ const router = createBrowserRouter(
                 {
                     "path": "/channels/:id",
                     "element":
+                    <SseCommunicationServiceProvider>
                         <ChannelServiceProvider>
                             <ChannelView/>
                         </ChannelServiceProvider>
+                    </SseCommunicationServiceProvider>,
                 }
             ]
         },
