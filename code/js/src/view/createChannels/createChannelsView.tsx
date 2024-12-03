e3a29ae821a11c1ac0e6da867fd46d5a70e9a825
mariana-moraes <A50560@alunos.isel.pt>
2024-12-03 02:42:10 +0000

    createChannelsView first implementation

---
+++ code/js/src/view/createChannels/createChannelsView.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function CreateChannelsView(): React.JSX.Element {
+    return (
+        <div>
+            <h1>Create Channels :D</h1>
+        </div>
+    )
+}
