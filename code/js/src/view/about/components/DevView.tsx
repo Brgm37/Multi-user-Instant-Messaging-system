ff797198fd222043cabc5394893782692a30c7e7
mariana-moraes <A50560@alunos.isel.pt>
2024-12-03 02:36:03 +0000

    Dev View alterations

--- code/js/src/view/about/components/DevView.tsx
+++ code/js/src/view/about/components/DevView.tsx
@@ -1,30 +1,35 @@
 import * as React from 'react'
 import {Dev} from "../hooks/states/AboutState";
+import {useAboutService} from "../hooks/UseAboutForm";
 
 
-export function DevView({ dev }: { dev: Dev }): React.JSX.Element  {
-    const [showDevInfo, setShowDevInfo] = React.useState(false)
-    const handleToggleDevInfo = () => setShowDevInfo(!showDevInfo)
+export function DevView(
+    {dev}: { dev: Dev}): React.JSX.Element  {
+    const [state, handler] = useAboutService()
+    const onClick = (dev: Dev) => {
+        handler.onSelectDev(dev)
+    }
+
     return (
         <div>
-            <button onClick={handleToggleDevInfo}>
+            <button onClick={() => onClick(dev)}>
                 <img
-                    src={dev.imageURL} // Placeholder if no image
-                    alt={`${dev.name}'s Profile Picture`}
+                    src={dev.imageURL}
                     style={{width: "30px", height: "30px", borderRadius: "50%"}}
                 />
-                {dev.name}</button>
-            {showDevInfo ? (
+                {dev.name}
+            </button>
+            {state.tag === "devView" && (
                 <>
                     <p>{dev.num}</p>
                     <p>{dev.bio || null}</p>
                     <p>{dev.email}</p>
-                    <a href={dev.github.href}>Github</a>
-                    <p></p>
-                    <a href={dev.linkdIn.href}>LinkedIn</a>
+                    <a href={dev.github.href} onClick={handler.onAcessGithub}>GitHub</a>
+                    <br/>
+                    <a href={dev.linkdIn.href} onClick={handler.onAcessLinkedIn}>LinkedIn</a>
                 </>
-            ) : null}
-            <p></p>
+            )}
         </div>
+
     )
 }
