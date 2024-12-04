6cccbbb9f24c0e27716f56884818322ffd13a599
mariana-moraes <A50560@alunos.isel.pt>
2024-12-04 02:31:31 +0000

    UsecreateChannelsForm update

--- code/js/src/view/createChannels/hooks/UseCreateChannelForm.ts
+++ code/js/src/view/createChannels/hooks/UseCreateChannelForm.ts
@@ -1,6 +1,16 @@
 import {CreateChannelsState, makeInitialState} from "./states/createChannelsState";
 import {CreateChannelsAction} from "./states/createChannelsAction";
-import {useReducer} from "react";
+import {useContext, useEffect, useReducer} from "react";
+import {CreateChannelsServiceContext} from "../../../service/createChannels/createChannelsServiceContext";
+import {isFailure} from "../../../model/Either";
+import {UseCreateChannelFormHandler} from "./handler/UseCreateChannelFormHandler";
+
+/**
+ * The delay for debounce.
+ */
+const DEBOUNCE_DELAY = 500;
+
+const ERROR_MESSAGE = "Channel name already exists"
 
 function reduce(state: CreateChannelsState, action: CreateChannelsAction): CreateChannelsState {
     switch (state.tag) {
@@ -49,7 +59,37 @@ function reduce(state: CreateChannelsState, action: CreateChannelsAction): Creat
 
 export function useCreateChannelForm(): [CreateChannelsState,CreateChannelsAction] {
     const [state, dispatch] = useReducer(reduce, makeInitialState())
-    return [state, dispatch]
+    const service = useContext(CreateChannelsServiceContext)
+    useEffect(() => {
+        if (state.tag !== "editing") return
+        const timeout = setTimeout(() => {
+            const fetchChannel=
+                state.input.name !== "" ?
+                service.findChannelByName(state.input.name) :
+                null
+            fetchChannel
+                .then(response => {
+                    if (isFailure(response)) dispatch({type: "error", error: response.value})
+                    else dispatch({type: "validation-result", response: response.value.toString()})
+                })
+        }, DEBOUNCE_DELAY)
+        return () => clearTimeout(timeout)
+    }, [state.tag]);
+
+    const handler: UseCreateChannelFormHandler = {
+        onNameChange(name: string) {
+            service
+                .findChannelByName(name)
+                .then(response => {
+                    if (response.tag === "success"){
+                        dispatch({type: "error", error: ERROR_MESSAGE})
+                    }else null
+                })
+            dispatch({type: "edit", input: "name", inputValue: name})
+        }
+        
+    }
+    return [state, handler]
 }
 
 
