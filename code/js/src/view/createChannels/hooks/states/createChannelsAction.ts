cfa1ce28202343004042f5d8ddd88411a412be68
mariana-moraes <A50560@alunos.isel.pt>
2024-12-05 02:54:33 +0000

    createChannelsAction update

--- code/js/src/view/createChannels/hooks/states/createChannelsAction.ts
+++ code/js/src/view/createChannels/hooks/states/createChannelsAction.ts
@@ -1,7 +1,7 @@
+import {ChannelInput} from "./createChannelsState";
 
 export type CreateChannelsAction =
     { type: "edit", input: "name" | "visibility" | "access", inputValue: string} |
-    { type: "error", error: string} |
-    { type: "success" } |
-    { type: "submit" } |
-    { type: "validation-result", response: string | true }
+    { type: "error", message: string} |
+    { type: "success", input: ChannelInput } |
+    { type: "submit" }
