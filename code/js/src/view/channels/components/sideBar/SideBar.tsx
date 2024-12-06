196cea704dd740fbac9688648aa2736d628da229
Brgm37 <brgm37@gmail.com>
2024-12-06 20:21:26 +0000

    Side bar changes

--- code/js/src/view/channels/components/sideBar/SideBar.tsx
+++ code/js/src/view/channels/components/sideBar/SideBar.tsx
@@ -1,53 +1,105 @@
 import * as React from "react";
 import InfiniteScroll from "../../../components/infiniteScroll/InfiniteScroll";
-import {useContext, useEffect} from "react";
-import {ChannelsServiceContext} from "../../../../service/channels/ChannelsServiceContext";
-import {Channel} from "../../../../model/Channel";
-import {useNavigate} from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { ChannelsServiceContext } from "../../../../service/channels/ChannelsServiceContext";
+import { Channel } from "../../../../model/Channel";
+import {Link, useNavigate} from "react-router-dom";
 
 const TIMEOUT = 500;
 
 export function Sidebar(): React.JSX.Element {
-    const [channels, setChannels] = React.useState<Channel[]>([])
-    const [search, setSearch] = React.useState<string>("")
-    const {findChannelsByName} = useContext(ChannelsServiceContext)
-    const onSearchChange = (value: React.ChangeEvent<HTMLInputElement>) => setSearch(value.target.value)
-    const navigate = useNavigate()
+    const [channels, setChannels] = useState<Channel[]>([]);
+    const [search, setSearch] = useState<string>("");
+    const [isInputVisible, setInputVisible] = useState<boolean>(false);
+    const { findChannelsByName } = useContext(ChannelsServiceContext);
+    const navigate = useNavigate();
+
+    const onSearchChange = (value: React.ChangeEvent<HTMLInputElement>) => setSearch(value.target.value);
+
     useEffect(() => {
         const timeout = setTimeout(() => {
             findChannelsByName(search, 0, 15)
-                .then(response => {if (response.tag === "success") setChannels(response.value)})
+                .then(response => {
+                    if (response.tag === "success") setChannels(response.value);
+                });
         }, TIMEOUT);
         return () => clearTimeout(timeout);
     }, [search]);
+
     useEffect(() => {
         const timeout = setTimeout(() => {
-            const c = channels.find(channel => channel.name === search)
-            if (c) navigate(`/channels/${c.id}`)
+            const c = channels.find(channel => channel.name === search);
+            if (c) {
+                navigate(`/channels/${c.id}`);
+                clearSearch();
+            }
         }, TIMEOUT);
         return () => clearTimeout(timeout);
     }, [search]);
+
+    const toggleInput = () => {
+        setInputVisible(!isInputVisible);
+    };
+
+    const clearSearch = () => {
+        setSearch("");
+        setInputVisible(false);
+    }
+
     return (
-        <div className="w-64 bg-gray-900 text-white flex flex-col">
-            <div className={"bg-gray-700 text-white p-2 rounded ml-auto w-full"}>
+        <div className="relative w-20 bg-gray-900 flex flex-col items-center py-4 space-y-4">
+            <div
+                className="w-14 h-14 overflow-hidden rounded-full transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-lgr cursor-pointer"
+                onClick={toggleInput}
+            >
+                <img
+                    src="/sideBarIcons/black_search.webp"
+                    alt="Search"
+                    className="w-14 h-14 rounded-full"
+                />
+            </div>
+
+            <div
+                className={`absolute w-40 top-0 left-20 bg-gray-700 text-white p-2 rounded transition-all duration-300 ease-in-out ${isInputVisible ? 'opacity-100 z-10' : 'opacity-0 z-0 pointer-events-none'}`}>
                 <input
                     type="text"
                     value={search}
                     onChange={onSearchChange}
-                    placeholder="Search for channels"
-                    className="bg-gray-700 text-white text-center w-full"
-                    list="sugesstions"
+                    placeholder="Search your channel"
+                    className="bg-gray-700 text-white text-center w-full w-40 h-6 p-2"
+                    list="suggestions"
                 />
                 <datalist
-                    id="sugesstions"
-                    className={"bg-pink-700 text-white p-2 rounded ml-auto w-full"}
+                    id="suggestions"
+                    className={"bg-pink-700 text-white p-2 rounded ml-auto w-full "}
                 >
-                    {channels.map(channel => (<option key={channel.id} value={channel.name}/>))}
+                    {channels.map(channel => (
+                        <option key={channel.id} value={channel.name}/>
+                    ))}
                 </datalist>
             </div>
+
+            <Link to={"/channels/findChannels"} className={"relative group"}>
+                <div
+                    className="w-14 h-14 overflow-hidden rounded-full transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-lg">
+                    <img
+                        src={"/sideBarIcons/findChannels.png"}
+                        alt={"findChannels"}
+                        className="w-full h-full object-cover object-center"/>
+                </div>
+                <div
+                    className="absolute left-16 top-1/2 transform -translate-y-1/2 z-50 bg-black text-white text-sm font-bold px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 tooltip-arrow pointer-events-none group-hover:pointer-events-auto"
+                    style={{width: "max-content"}}
+                >
+                    Find Channels
+                </div>
+            </Link>
+
+            <div className="w-12 h-0.5 bg-gray-700"></div>
+
             <InfiniteScroll
-                className="flex-1 overflow-y-auto"
-                scrollStyle="custom-scrollbar"
+                className="flex-1 w-20"
+                scrollStyle="custom-scrollbar space-y-2"
             />
         </div>
     );
