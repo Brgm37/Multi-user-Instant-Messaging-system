43a636cc822a18e7da7c1af16a58fde74cc74f99
mariana-moraes <A50560@alunos.isel.pt>
2024-12-06 19:50:51 +0000

    create channel actions go back action created

--- code/js/src/view/createChannels/hooks/states/createChannelsAction.ts
+++ code/js/src/view/createChannels/hooks/states/createChannelsAction.ts
@@ -1,7 +1,8 @@
 import {ChannelInput} from "./createChannelsState";
 
 export type CreateChannelsAction =
-    { type: "edit", input: "name" | "visibility" | "access", inputValue: string} |
+    { type: "edit", inputValue: string} |
     { type: "error", message: string} |
     { type: "success", input: ChannelInput } |
-    { type: "submit" }
+    { type: "submit" } |
+    { type: "go-back" }
