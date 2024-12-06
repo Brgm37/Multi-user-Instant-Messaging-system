73172625845337fce17333a5feaf5e448b383e1d
mariana-moraes <A50560@alunos.isel.pt>
2024-12-06 19:49:25 +0000

    create channel view uodate

--- code/js/src/view/createChannels/createChannelsView.tsx
+++ code/js/src/view/createChannels/createChannelsView.tsx
@@ -3,8 +3,7 @@ import {useCreateChannel} from "./hooks/UseCreateChannel";
 import {CreateChannelsState} from "./hooks/states/createChannelsState";
 import {UseCreateChannelHandler} from "./hooks/handler/UseCreateChannelHandler";
 import {Navigate} from "react-router-dom";
-import {SearchBar} from "../components/SearchBar";
-import {ToggleCreateChannelsView} from "./components/ToggleCreateChannelView";
+import {CreateChannelsBaseView} from "./components/CreateChannelsBaseView";
 
 export function CreateChannelsView(): React.JSX.Element {
     const [state, handler]: [CreateChannelsState, UseCreateChannelHandler] = useCreateChannel()
@@ -15,17 +14,16 @@ export function CreateChannelsView(): React.JSX.Element {
                 return <Navigate to={"/channels"}/>
             case "error":
                 return <div>{state.message}</div>
+            case "submitting":
+                return <div>Submitting...</div>
+            case "editing":
+                return <CreateChannelsBaseView handler={handler} state={state} onGenerate={handler.onSubmit}/>
         }
     })
     return (
         <div>
             <h1>Create Channel</h1>
-            <SearchBar
-                value={state.input.name}
-                onChange={handler.onNameChange}
-                placeholder={"Channel Name"}
-                className={"bg-gray-700 text-white p-2 rounded"}/>
-            <ToggleCreateChannelsView state={state} onGenerate={handler.onSubmit}></ToggleCreateChannelsView>
+            {view(state)}
         </div>
     )
 }
