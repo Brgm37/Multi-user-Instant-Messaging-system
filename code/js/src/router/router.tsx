1f44fe468dc355bc649f6501d55e7ab5cc45dcff
Brgm37 <brgm37@gmail.com>
2024-12-07 11:39:32 +0000

    imagePicker added

--- code/js/src/router/router.tsx
+++ code/js/src/router/router.tsx
@@ -15,7 +15,6 @@ import {RegisterServiceProvider} from "../service/registration/register/Register
 import {ChannelServiceProvider} from "../service/channel/ChannelServiceProvider";
 import {AuthValidator} from "../view/session/authValidator";
 import {FindChannelsView} from "../view/findChannels/FindChannelsView";
-import {FindChannelsMockServiceProvider} from "../service/findChannels/mock/FindChannelsMockServiceProvider";
 import {AboutView} from "../view/about/AboutView";
 import {CreateChannelsView} from "../view/createChannels/createChannelsView";
 import {ChannelsView} from "../view/channels/ChannelsView";
@@ -31,6 +30,8 @@ import {
     CreateChannelInvitationServiceProvider
 } from "../service/createChannelInvitation/CreateChannelInvitationServiceProvider";
 import {CreateChannelServiceProvider} from "../service/createChannels/createChannelsServiceProvider";
+import {ImagePickerProvider} from "../view/components/ImagePicker/ImagePickerProvider";
+import ImagePicker from "../view/components/ImagePicker/ImagePicker";
 
 const router = createBrowserRouter(
     [
@@ -97,25 +98,22 @@ const router = createBrowserRouter(
                             <FindChannelsView/>
                         </FindChannelsServiceProvider>,
                 },
-            ]
-                },
                 {
                     "path": "/channels/createChannel",
                     "element":
                         <CreateChannelServiceProvider>
-                            <CreateChannelsView/>
-                        </CreateChannelServiceProvider>
+                            <ImagePickerProvider>
+                                <CreateChannelsView/>
+                                <ImagePicker/>
+                            </ImagePickerProvider>
+
+                        </CreateChannelServiceProvider>,
+                },
+            ]
                 },
         {
             "path": "/about",
             "element": <AboutView />,
-        },
-        {
-            "path": "/findChannels",
-            "element":
-                <FindChannelsMockServiceProvider>
-                    <FindChannelsView/>
-                </FindChannelsMockServiceProvider>,
         },
     ]
 )
